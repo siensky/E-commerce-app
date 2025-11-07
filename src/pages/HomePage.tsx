1c133bbef8973c58ee7913beca5a9ec283a5cffe
siennalansky <siennalansky@gmail.com>
2025-11-07 16:05:41 +0100

    fetch featured products

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+
 function HomePage() {
+  const [featured, setFeatured] = useState({});
+
+  useEffect(() => {
+    async function fetchFeaturedProduct() {
+      await fetch("https://play-ecom-api.allcodeapp.com/api/featured").then(
+        async (data) => {
+          const response: any = await data.json();
+          console.log(response);
+        }
+      );
+    }
+    fetchFeaturedProduct();
+  }, []);
   return (
     <>
-      <h1>Home</h1>
+      <p></p>
     </>
   );
 }
